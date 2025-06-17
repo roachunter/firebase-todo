d9d01f709d875fb0a691cebe2c4ce2c984b22814
Danone <mmmdanone04@gmail.com>
2025-06-17 10:06:37 +0300

    add navigation deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.auth)
+    implementation(libs.navigation.compose)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
